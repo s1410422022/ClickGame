bbdb639c4eb1fc8e84cd18605ebc9ecacdd7cc9d
4419-2-4\user <kk159357222@gmail.com>
2017-12-23 10:49:47 +0800

    1.新增EnemyBehavior 2.完成MeshFader

---
+++ Assets/Scripts/View/Behavior/EnemyBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]//屬性>>要有它存在
+
+public class EnemyBehavior : MonoBehaviour {
+
+	private Animator animator;
+
+	private void Awake()
+	{
+		animator = GetComponent<Animator>();
+
+	}
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/View/Component/MeshFader.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshFader : MonoBehaviour {
+
+	//淡入淡出
+	public Renderer[] fadeRenderers;
+	public float speed = 1f;
+
+	[ContextMenu("Test FadeIn")]
+	private void Test()
+	{
+		StartCoroutine (FadeIn());
+	}
+
+	[ContextMenu("Test2 FadeOut")]
+	private void Test2()
+	{
+		StartCoroutine (FadeOut());
+	}
+
+
+
+	public IEnumerator FadeIn()
+	{
+		StopCoroutine (FadeOut());
+		float alpha = 0;
+		for (int i = 0; i < fadeRenderers.Length; i++)
+		{
+			Color color = fadeRenderers [i].material.color;
+			color.a = alpha;
+			fadeRenderers [i].material.color = color;
+		}
+
+		while (alpha < 1) 
+		{
+			alpha += Time.deltaTime * speed;
+			alpha = Mathf.Min (1, alpha); //ALPHA1.1>>取最小值1>>變回1
+			ChangeAlpha(alpha);
+			yield return null;
+		}
+	}
+
+
+	public IEnumerator FadeOut()
+	{
+		StopCoroutine (FadeIn());
+		float alpha = 1;
+		for (int i = 0; i < fadeRenderers.Length; i++)
+		{
+			Color color = fadeRenderers [i].material.color;
+			color.a = alpha;
+			fadeRenderers [i].material.color = color;
+		}
+
+		while (alpha > 0) 
+		{
+			alpha -= Time.deltaTime * speed;
+			alpha = Mathf.Max (0, alpha); //ALPHA1.1>>取最小值1>>變回1
+			ChangeAlpha(alpha);
+			yield return null;
+		}
+	}
+
+
+
+	private void ChangeAlpha(float alpha){
+		for (int i = 0; i < fadeRenderers.Length; i++)
+		{
+			Color color = fadeRenderers [i].material.color;
+			color.a = alpha;
+			fadeRenderers [i].material.color = color;
+		}
+	}
+
+
+}
